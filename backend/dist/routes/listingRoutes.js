4e1f78dd8a49fe31db7b13d1e41dc1d8b899f578
ShivamSingh0111 <rajputshivamsinghrkt98@gmail.com>
2026-09-18 16:48:23 +0530

    chore: remove tracked node_modules and dist folders from git

--- backend/dist/routes/listingRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.listingRouter = void 0;
-const express_1 = require("express");
-const listingController_js_1 = require("../controllers/listingController.js");
-exports.listingRouter = (0, express_1.Router)();
-exports.listingRouter.get('/listing', listingController_js_1.ListingController.getListing);
-exports.listingRouter.get('/listing/photos', listingController_js_1.ListingController.getPhotos);
-exports.listingRouter.get('/listing/reviews', listingController_js_1.ListingController.getReviews);
--- backend/dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_js_1 = require("./app.js");
-const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 5000;
-const app = (0, app_js_1.createApp)();
-app.listen(PORT, () => {
-    console.log(`Airbnb Listing API server running at http://localhost:${PORT}`);
-});
